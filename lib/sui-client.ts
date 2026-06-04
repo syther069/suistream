45f97d619ab7b05961d34ed3914e375fd7b275a3
syther069 <testnetairdrop6969@gmail.com>
2026-06-04 19:29:52 +0530

    fix: disable eslint any warning in sui-client.ts

--- lib/sui-client.ts
+++ lib/sui-client.ts
@@ -1,6 +1,7 @@
 export const SUI_RPC_PROXY_PATH = "/api/sui-rpc";
 
-export const suiClient: any = null;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const suiclient: any = null;
 
 export function createSuiClient() {
   return null;
